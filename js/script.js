3bd8a09aedc7c68a130e77d538ae36f039b8952f
walter21gt <fabriciogt009@gmail.com>
2019-06-04 17:52:36 -0500

    apl

--- js/script.js
+++ js/script.js
@@ -3,10 +3,14 @@ var Pokemon = angular.module("Pokemon",[])
 
 Pokemon.controller("ListadoPokemon", function($scope,$rootScope,$http) {
 	$scope.Pokebola = [];
+	for(var m=1;m<=500;m++)
 	$http ({
 		method: "GET",
-		url: "https://pokeapi.co/api/v2/pokemon/100"
+		//01110111 01100001 01101100 01110100 01100101 01110010 00100000 01000111 01100001 01100010 01110010 01101001 01100101 01101100
+		url: "https://pokeapi.co/api/v2/pokemon/"+m
 	}).then(function success(x){
 		$scope.Pokebola.push(x);
 	})
 })
+
+
